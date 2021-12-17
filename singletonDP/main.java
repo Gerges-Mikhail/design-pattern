9f70d6bf454905677ce17f7ecf63e6e3b061b7a5
Ggerges Mikhail <68674811+Gerges-Mikhail@users.noreply.github.com>
2021-12-17 14:39:09 +0200

    Add files via upload

---
+++ singletonDP/main.java
@@ -0,0 +1,11 @@
+package singletonDP;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		SingleObject singleO = SingleObject.getInstance();
+		singleO.showMessage();
+	}
+
+}
